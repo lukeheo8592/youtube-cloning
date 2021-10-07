176a47896d66ae6651a7e96b2d44806817fef82c
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-10-07 00:36:27 -0400

    add views

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,9 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("upload");
+export const deleteVideo = (req, res) => {
+    console.log(req.params);
+    res.send("delete Video");
+};
