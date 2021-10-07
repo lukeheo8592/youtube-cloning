cc064f317404046a15827d6c56c510eb7bee6537
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-10-06 20:29:00 -0400

    clean the server code
    clean the server code

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter  from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-
-const handlehome = (req,res) =>{
-
-}
-
 app.use(logger);
-app.get("/", handlehome);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => console.log(`server listening on port http://localhost:${PORT}`)
 
