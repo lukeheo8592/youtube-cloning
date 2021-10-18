84ece8bf4476084e8b7cc6beaa0f9c6ae9691b16
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-10-18 17:00:43 -0400

    add postedit

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,15 +1,12 @@
 import express from "express";
-import { postEdit,watch, getEdit,deleteVideo, upload} from "../controllers/videoController";
+import { postEdit,watch, getEdit} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.post("/:id(\\d+)/edit", postEdit);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 
 export default videoRouter;
