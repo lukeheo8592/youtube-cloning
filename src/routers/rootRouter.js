c252cce453a9c74a76be7fa22664dba3ee8a9252
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-10-22 18:18:48 -0400

    add user auth

---
+++ src/routers/rootRouter.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {postJoin,getJoin, login} from "../controllers/userController";
+import { search,home } from "../controllers/videoController";
+
+const rootRouter = express.Router();
+
+
+rootRouter.get("/", home);
+rootRouter.route("/join").get(getJoin).post(postJoin);
+rootRouter.get("/login", login);
+rootRouter.get("/search", search);
+
+
+
+
+
+
+export default rootRouter;
