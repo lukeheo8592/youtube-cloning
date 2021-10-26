14f49c2cf6dc413ae9eb13368605093e28f588e6
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-10-26 00:30:06 -0400

    env

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,6 +2,5 @@ export const localsmiddleware = (req, res, next) =>{
     res.locals.siteName = "wetube";
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.loggedInUser = req.session.user;
-    console.log(res.locals);
     next();
 }
