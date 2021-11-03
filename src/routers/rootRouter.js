3ee933be0db384233d58a49233c68ba43c9d2d8b
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-11-03 16:55:25 -0400

    protector and public middlewares

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,13 +1,18 @@
 import express from "express";
 import {postLogin,postJoin,getJoin, getLogin} from "../controllers/userController";
 import { search,home } from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter
+  .route("/login")
+  .all(publicOnlyMiddleware)
+  .get(getLogin)
+  .post(postLogin);
 rootRouter.get("/search", search);
 
 
