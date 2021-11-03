965017523c978014b88db6afc89984a0da368db8
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-11-02 23:17:54 -0400

    add edit-profile page

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,6 @@
 export const localsmiddleware = (req, res, next) =>{
     res.locals.siteName = "wetube";
     res.locals.loggedIn = Boolean(req.session.loggedIn);
-    res.locals.loggedInUser = req.session.user;
+    res.locals.loggedInUser = req.session.user || {};
     next();
 }
