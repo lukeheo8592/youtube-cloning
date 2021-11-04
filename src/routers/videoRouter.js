b21ca0c9d498c167542c4534101a5dc325333d77
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-11-04 00:30:30 -0400

    add video upload

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { deleteVideo,postUpload,getUpload,postEdit,watch, getEdit} from "../controllers/videoController";
-import { protectorMiddleware } from "../middlewares";
+import { protectorMiddleware, videoUpload } from "../middlewares";
 const videoRouter = express.Router();
 
 
@@ -19,7 +19,7 @@ videoRouter
   .route("/upload")
   .all(protectorMiddleware)
   .get(getUpload)
-  .post(postUpload);
+  .post(videoUpload.single("video"), postUpload);
 
 
 export default videoRouter;
