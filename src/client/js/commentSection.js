3a36f6ebc4f0b5218c77b63151186fb5c829206d
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-11-11 16:57:52 -0500

    prepare to delete comment

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,18 +1,22 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const addComment = (text) => {
-    const videoComments = document.querySelector(".video__comments ul");
-    const newComment = document.createElement("li");
-    newComment.className = "video__comment";
-    const icon = document.createElement("i");
-    icon.className = "fas fa-comment";
-    const span = document.createElement("span");
-    span.innerText = ` ${text}`;
-    newComment.appendChild(icon);
-    newComment.appendChild(span);
-    videoComments.prepend(newComment);
-  };
+const addComment = (text, id) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.dataset.id = id;
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  const span2 = document.createElement("span");
+  span2.innerText = "❌";
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  newComment.appendChild(span2);
+  videoComments.prepend(newComment);
+};
 
 const handleSubmit = async (event) => {
   event.preventDefault();
@@ -22,19 +26,20 @@ const handleSubmit = async (event) => {
   if (text === "" || text.trim() === "") {
     return;
   }
-  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ text }),
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }),
   });
-  textarea.value = "";
-  if (status === 201) {
-    addComment(text);
+  if (response.status === 201) {
+    textarea.value = "";
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
   }
 };
 
 if (form) {
-    form.addEventListener("submit", handleSubmit);
-  }
+  form.addEventListener("submit", handleSubmit);
+}
