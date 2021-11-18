926b56bfccb1eb6428ca88c69e067231ae7c3db1
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2021-11-17 19:50:07 -0500

    revise video

--- src/models/video.js
+++ src/models/video.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 80 },
   fileUrl: { type: String, required: true },
